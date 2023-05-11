5abf58a776235c37af460ebdc760edf12081a577
马雅狄 <ma.athenadiana@gmail.com>
2023-05-11 13:54:09 +0800

    gcov branch coverage 66.7% initially.

---
+++ gcc.sh
@@ -0,0 +1,2 @@
+gcc -fprofile-arcs -ftest-coverage -fpic -o0 $1 -o $2
+./$2
---
+++ gcovr.sh
@@ -0,0 +1,5 @@
+#/home/mayadi/unit_test_package/gcovr-3.3/scripts/gcovr -r $1 –b
+/home/mayadi/unit_test_package/gcovr-3.3/scripts/gcovr -r $1 --html --html-details -o $2.html
+
+#/home/mayadi/unit_test_package/gcovr-3.3/scripts/gcovr -r . --xml-pretty
+#/home/mayadi/unit_test_package/gcovr-3.3/scripts/gcovr -r . --html -o application.html
